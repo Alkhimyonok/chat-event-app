02061e6dbde3026302eaa29468fca3f8c5eefa79
VeronikaAlkhimenok <alkhimyonok.v@gmail.com>
2022-10-25 15:44:41 +0400

    fix pr comments

--- force-app/main/default/chat/lwc/chat/chat.js
+++ force-app/main/default/chat/lwc/chat/chat.js
@@ -15,6 +15,7 @@ const fields = [MESSAGE_ID_FIELD, MESSAGE_BODY_FIELD, MESSAGE_USER_ALIAS_FIELD, 
 
 import EVENT_MESSAGE_ID_FIELD from "@salesforce/schema/Chat_Event__e.Message_Id__c";
 const CHANNEL_NAME = '/event/Chat_Event__e';
+const BROADCAST_AFTER_SUBSCRIBE_REPLAY_ID = -1;
 
 import {MessageWrapperBuilder} from "c/messageItem";
 
@@ -51,12 +52,11 @@ export default class Chat extends LightningElement {
     }
 
     subscribeOnChatEvent() {
-        const thisRef = this;
-        const receiveMessageHandler = function (response) {
-            thisRef.receivedMessageId = response.data.payload[EVENT_MESSAGE_ID_FIELD.fieldApiName];
+        const receiveMessageHandler = (response) => {
+            this.receivedMessageId = response.data.payload[EVENT_MESSAGE_ID_FIELD.fieldApiName];
         };
 
-        subscribe(CHANNEL_NAME, -1, receiveMessageHandler).then(response => {
+        subscribe(CHANNEL_NAME, BROADCAST_AFTER_SUBSCRIBE_REPLAY_ID, receiveMessageHandler).then(response => {
             this.subscription = response;
         });
     }
